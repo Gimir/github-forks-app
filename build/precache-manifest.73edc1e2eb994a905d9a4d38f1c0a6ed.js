2084a49def0c297dadbe87396990619c5453f62e
Gimir <mukhammad.amaev@mail.ru>
2019-12-19 16:17:27 +0300

    Heroku

---
+++ build/precache-manifest.73edc1e2eb994a905d9a4d38f1c0a6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b999733d77fd3503e1556b55de5b67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d1808421a1e83c009c",
+    "url": "/static/css/main.77659192.chunk.css"
+  },
+  {
+    "revision": "22ddcbec511dd598f4d4",
+    "url": "/static/js/2.279a65d9.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/static/js/2.279a65d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20d1808421a1e83c009c",
+    "url": "/static/js/main.32db175f.chunk.js"
+  },
+  {
+    "revision": "eca3a6c2645f55641cce",
+    "url": "/static/js/runtime-main.bfa63022.js"
+  }
+]);
